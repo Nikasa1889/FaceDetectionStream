c7ff5e4712e6a988410badb7c2a582a879d87f46
Deep Learning <chihieu392@gmail.com>
2017-08-24 01:47:35 -0700

    test deploy script

---
+++ deploy.py
@@ -0,0 +1,15 @@
+"""
+Run command to push to git
+"""
+from subprocess import Popen, PIPE
+message = "test deploy script"
+
+
+p = Popen(["git", "add", "--all"])
+p.communicate()
+
+p = Popen(["git", "commit", "-m", message])
+p.communicate()
+
+p = Popen(["git", "push", "--force", "origin", "HEAD:master"])
+p.communicate()
